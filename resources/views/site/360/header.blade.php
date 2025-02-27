4c904c929544ae5ef81f675fccdcacc84a8a0d08
Italo-Emidio <italo.emidio7@gmail.com>
2025-02-27 14:37:41 -0300

    #22 Foi realizado a alteração nos botões Entrar e Cadastrar do header

--- resources/views/site/360/header.blade.php
+++ resources/views/site/360/header.blade.php
@@ -4,7 +4,7 @@
             <img src="{{asset('img/logo_listados_360.png')}}" height="98px" margin-left="42px" style="margin-left: 290px"> 
         </a>
 
-    <div class="header-container position-relative d-flex align-items-center justify-content-between" style="background: #073a59; color: #FFFFFF">
+    <div class="header-container position-relative d-flex align-items-center justify-content-between" style="background: #073a59; color: #FFFFFF; margin: 0 auto; height: 52px">
 
         
 
@@ -19,9 +19,9 @@
             </ul>
             <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
         </nav>
-        <nav>
-            <a href="https://360.listados.com.br/backend/login.php" target="_blank">Entrar</a>
+    </div>
+        <nav style="margin-right: 290px">
+            <a href="https://360.listados.com.br/backend/login.php" target="_blank" style="color: #050414">Entrar</a>
             <a class="btn-getstarted" href="https://360.listados.com.br/backend/register.php" target="_blank">Cadastrar</a>
         </nav>
-    </div>
 </header>
